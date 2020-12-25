d23e685970e8ff957c2f85e3ab40cc64b7b10d75
Vineet Raj Parashar <vineetrajparashar25@gmail.com>
2020-12-25 16:44:59 +0530

    Add files via upload

---
+++ 9.Bitwise Operations (bitwise AND, OR, NOR).py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+img1=np.zeros((405,720,3),np.uint8)
+img1=cv2.rectangle(img1,(200,0),(300,100),(255,255,255),-1)
+img2=cv2.imread('messi.jpg')
+#bitAnd=cv2.bitwise_and(img2, img1)
+#bitOr=cv2.bitwise_or(img2,img1)
+#bitXor=cv2.bitwise_xor(img2,img1)
+bitNot1=cv2.bitwise_not(img1)
+bitNot2=cv2.bitwise_not(img2)
+cv2.imshow('img1',img1)
+cv2.imshow('img2',img2)
+cv2.imshow('New',bitNot2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
