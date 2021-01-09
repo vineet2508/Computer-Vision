aee016f9c004db80f08f87a334a338747b670b02
Vineet Raj Parashar <vineetrajparashar25@gmail.com>
2021-01-10 00:11:40 +0530

    Add files via upload

---
+++ 32.Road Lane Line Detection with OpenCV1.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+image = cv2.imread('road.jpg')
+image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+
+print(image.shape)
+height = image.shape[0]
+width = image.shape[1]
+region_of_interest_vertices = [(0,height),(width/2,height/2),(width,height)]
+def region_of_interest(img,vertices):
+    mask = np.zeros_like(img)
+    channel_count = img.shape[2]
+    match_mask_color = (255,) * channel_count
+    cv2.fillPoly(mask,vertices,match_mask_color)
+    masked_image = cv2.bitwise_and(img,mask)
+    return masked_image
+cropped_img = region_of_interest(image,np.array([region_of_interest_vertices],np.int32))
+plt.imshow(cropped_img)
+plt.show()
